b772ce220516c9281bb9d3921042397e50c66fe0
Tom Hanley <thanley11@gmail.com>
2020-05-24 11:52:21 -0400

    Using schematics and dto class, returning bookings on post

--- python/app.py
+++ python/app.py
@@ -1,7 +1,38 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import requests
 
+from schematics.models import Model
+from schematics.types import IntType, StringType
+from schematics.types.compound import ModelType, ListType
+
+from schematics.exceptions import ModelValidationError
+from werkzeug.exceptions import HTTPException
+from collections import defaultdict
 
 app = Flask(__name__)
 CORS(app)
+
+user_bookings = defaultdict(list)
+
+
+class BookingDto(Model):
+    name = StringType(required=True)
+    time = StringType(required=True)
+    instructor = IntType(required=True)
+
+
+# @app.errorhandler(HTTPException)
+@app.route('/api/book-time/', methods=['POST'])
+def book_time():
+    try:
+        payload = request.get_json()
+        booking = BookingDto(payload)
+        booking.validate()
+        instructor_id = booking.get("instructor")
+        data = booking.to_primitive()
+        user_bookings[instructor_id].append(data)
+        return jsonify(user_bookings), 200
+    except ModelValidationError:
+        abort(400, 'Validation Error')
