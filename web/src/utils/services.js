d5bb4660544e0b718c87ca937409cf6e656f672c
Tom Hanley <thanley11@gmail.com>
2020-05-24 20:29:33 -0400

    Adding service call for get bookings

--- web/src/utils/services.js
+++ web/src/utils/services.js
@@ -1,6 +1,5 @@
 
-
-const bookTime  = async (params) => fetch(`http://localhost:4433/api/book-time/`, {
+const bookTime  = async (params) => fetch(`http://localhost:4433/api/booking/`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -16,4 +15,6 @@ const getAvailableTimes = async () => fetch(`${'https://cors-anywhere.herokuapp.
       },
 })
 
-export { bookTime, getAvailableTimes}
+const getBookingTimes = async () => fetch(`http://localhost:4433/api/booking/`);
+
+export { bookTime, getAvailableTimes, getBookingTimes}
