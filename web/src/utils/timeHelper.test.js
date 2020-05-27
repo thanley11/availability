5fcbaa2f06bc09ba88c08eb7027785b4f1398dc9
Tom Hanley <thanley11@gmail.com>
2020-05-27 14:27:14 -0400

    Adding flatten and convert tests

--- web/src/utils/timeHelper.test.js
+++ web/src/utils/timeHelper.test.js
@@ -1,24 +1,26 @@
-import { formatTime,sortTimesDateAsc, fetchToday } from './timeHelper';
+import { formatTime,sortTimesDateAsc, fetchToday, convertTimeData, flattenTimes } from './timeHelper';
 
 describe('timeHelper', () => {
    beforeAll(() => {
         Date.now = jest.fn(() => 1590173622627) //2020-05-22
    });
   const date = "2019-08-21T15:00:00-04:00";
-  const times = [
+  const timesArray = [
     "2019-08-25T10:30:00-04:00",
     "2019-08-24T10:30:00-04:00",
     "2019-08-25T11:00:00-04:00",
     "2019-08-24T11:00:00-04:00",
     "2019-08-24T20:00:00-04:00",
     "2019-08-25T20:00:00-04:00"
-  ]
+  ];
+  const timesDict = {"2019-08-27": {"2019-08-27T14:00:00-04:00": 372955, "2019-08-27T13:00:00-04:00": 372955 }};
+  const bookedTimes = {372955:["2019-08-27T14:00:00-04:00", "2019-08-27T13:00:00-04:00" ]};
 
   it('formatTime should format to "MM/dd/yyyy hh:mm a" format', () => {
     expect(formatTime(date)).toEqual("08/21/2019 03:00 PM")
   })
   it('sortTimesDateAsc should sort by date in desc', () => {
-    expect(sortTimesDateAsc(times)).toEqual( [
+    expect(sortTimesDateAsc(timesArray)).toEqual( [
         "2019-08-24T10:30:00-04:00",
         "2019-08-24T11:00:00-04:00",
         "2019-08-24T20:00:00-04:00",
@@ -30,4 +32,12 @@ describe('timeHelper', () => {
   it('fetchToday should return todays date and formatted in yyyy-MM-dd format', () => {
     expect(fetchToday()).toEqual("2020-05-22")
   })
+  it('convertTimeData should return an array from the object', () => {
+    expect(convertTimeData(timesDict)).toEqual(bookedTimes)
+  })
+  it('flattenTimes should return todays date and formatted in yyyy-MM-dd format', () => {
+    expect(flattenTimes(bookedTimes)).toEqual(["2019-08-27T14:00:00-04:00", "2019-08-27T13:00:00-04:00"]
+    )
+  })
+
 })
