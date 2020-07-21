683b1aa62f91688801e1eac3d0748ed653db8aaf
Tom Hanley <thanley11@gmail.com>
2020-07-20 20:58:30 -0400

    Removing node app

--- node/app.test.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-
-describe("today", () => {
-    it("returns today's formatted date", () => {
-        expect(app.today()).toBe(new Date().toLocaleDateString());
-    });
-});
